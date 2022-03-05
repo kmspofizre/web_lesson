fbf8044e49687f4c2218b91a8a4c2e28ccde6158
Kirill <89915686+kmspofizre@users.noreply.github.com>
2022-03-05 18:38:01 +0300

    Задача список профессий

--- index.py
+++ index.py
@@ -4,6 +4,9 @@ from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
 
+sp = ['инженер-исследователь', 'пилот', 'строитель']
+
+
 @app.route('/')
 @app.route('/index')
 def index():
@@ -19,6 +22,11 @@ def training(prof):
     param['spaceship_1'] = f"src={url_for('static', filename='img/spaceship_1.jpg')}"
     param['spaceship_2'] = f"src={url_for('static', filename='img/spaceship_2.jpg')}"
     return render_template('index.html', **param)
+
+
+@app.route('/list_prof/<list_type>')
+def list_maker(list_type):
+    return render_template('index.html', profs=sp, lt=list_type)
 
 
 if __name__ == '__main__':
